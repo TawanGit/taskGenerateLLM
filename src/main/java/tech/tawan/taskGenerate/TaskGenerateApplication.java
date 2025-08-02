f8b7f704572ae92d2fed610f997b343d476f5226
tawan <thiago_tawan@outlook.com>
2025-08-02 11:20:06 -0300

    initialize project with Maven and Spring Boot setup

---
+++ src/main/java/tech/tawan/taskGenerate/TaskGenerateApplication.java
@@ -0,0 +1,13 @@
+package tech.tawan.taskGenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskGenerateApplication.class, args);
+	}
+
+}
